9925f752e3999985e2c4d72d07b5bb63e9b77b08
Eric <menaericdaniel@gmail.com>
2022-08-16 17:46:10 -0300

    structure initial

---
+++ src/controllers/mainController.js
@@ -0,0 +1,31 @@
+const {loadProducts} = require('../data/productsModule');
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		// Do the magic
+		const products = loadProducts()
+		const inSale = products.filter(product => product.category === 'in-sale');
+		const visited = products.filter(product => product.category === 'visited')
+
+		return res.render('index',{
+			inSale,
+			visited,
+			toThousand
+		})
+	},
+	search: (req, res) => {
+		// Do the magic
+		const products = loadProducts();
+		const result = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()));
+
+		return res.render('results',{
+			products : result,
+			keywords : req.query.keywords,
+			toThousand
+		})
+	},
+};
+
+module.exports = controller;
---
+++ src/routes/main.js
@@ -0,0 +1,12 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+// ************ Controller Require ************
+const {index, search} = require('../controllers/mainController');
+
+router
+    .get('/', index)
+    .get('/search', search)
+
+module.exports = router;
